444f32359cb05b41e56443801ede9ead044f33d5
derwish-pro <derwish.pro@gmail.com>
2015-09-16 23:53:33 +0300

    WebController: sliders, switches update fix

--- WebController/Scripts/MySensors.js
+++ WebController/Scripts/MySensors.js
@@ -169,3 +169,17 @@
         I_GET_NONCE_RESPONSE: 17
     }
 }
+
+function hexFromRGB(r, g, b) {
+    var hex = [
+      r.toString(16),
+      g.toString(16),
+      b.toString(16)
+    ];
+    $.each(hex, function (nr, val) {
+        if (val.length === 1) {
+            hex[nr] = "0" + val;
+        }
+    });
+    return hex.join("").toUpperCase();
+}
