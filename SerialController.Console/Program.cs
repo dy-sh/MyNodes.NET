9a2b9f4655632bb09a7676cef44f10e3b85037b9
derwish-pro <derwish.pro@gmail.com>
2015-10-26 05:24:58 +0300

    SerialController.Console refactoring

---
+++ SerialController.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿/*  MyNetSensors 
+    Copyright (C) 2015 Derwish <derwish.pro@gmail.com>
+    License: http://www.gnu.org/licenses/gpl-3.0.txt  
+*/
+
+/*  MyNetSensors 
+    Copyright (C) 2015 Derwish <derwish.pro@gmail.com>
+    License: http://www.gnu.org/licenses/gpl-3.0.txt  
+*/
+
+namespace MyNetSensors.SerialController.Console
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            SerialController.OnDebugStateMessage += System.Console.WriteLine;
+            SerialController.OnDebugTxRxMessage += System.Console.WriteLine;
+            SerialController.Start();
+            while (true)
+                System.Console.ReadLine();
+        }
+    }
+}
