365c76faaf7c77b5f01a8bd3b04e2581f307d832
derwish-pro <derwish.pro@gmail.com>
2016-01-31 00:14:10 +0300

    events delay meter node added

---
+++ Libs/Nodes/Nodes/OperationEventsDelayMeterNode.cs
@@ -0,0 +1,44 @@
+﻿//planer-pro copyright 2015 GPL - license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MyNetSensors.Nodes
+{
+
+    public class OperationEventsDelayMeterNode : Node
+    {
+        DateTime lasTime;
+
+        public OperationEventsDelayMeterNode() : base(1, 1)
+        {
+            this.Title = "Events Delay Meter";
+            this.Type = "Operation/Events Delay Meter";
+
+            Outputs[0].Name = "Delay (ms)";
+
+            Inputs[0].Type = DataType.Text;
+            Outputs[0].Type = DataType.Number;
+
+            lasTime = DateTime.Now;
+        }
+
+        public override void Loop()
+        {
+        }
+
+
+        public override void OnInputChange(Input input)
+        {
+            double delay = (DateTime.Now - lasTime).TotalMilliseconds;
+            lasTime = DateTime.Now;
+
+            LogInfo($"[{delay}] ms");
+            Outputs[0].Value = delay.ToString();
+        }
+    }
+}
