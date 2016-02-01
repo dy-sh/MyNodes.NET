0e3368aca66ac87838d35259e48436884715a9bb
derwish-pro <derwish.pro@gmail.com>
2016-02-01 20:10:44 +0300

    speech node > messages buffer

--- WebController/wwwroot/js/dashboard/mynetsensors-dashboard-speech.js
+++ WebController/wwwroot/js/dashboard/mynetsensors-dashboard-speech.js
@@ -18,17 +18,36 @@ function createSpeech(node) {
 }
 
 
+var playlist = [];
+var audio = new Audio();
+
 function updateSpeech(node) {
     $('#speechName-' + node.Id).html(node.Name);
 
     if (node.Value == null || node.Value == "")
         return;
 
-    //var msg = new SpeechSynthesisUtterance(text);
-    //window.speechSynthesis.speak(msg);
+    playlist.push(node.Value);
 
-    var url = "https://tts.voicetech.yandex.net/generate?key=95134f06-ffa6-4e8d-8bf0-49f3d07a8918&text=" + node.Value;
-    var audio = new Audio(url);
-    audio.play();
+    if (audio.paused)
+        playNextTrack();
+
 }
 
+
+audio.addEventListener('ended', playNextTrack);
+
+function playNextTrack() {
+    if (playlist.length == 0)
+        return;
+
+    var text = playlist.shift();
+    var url = "https://tts.voicetech.yandex.net/generate?key=95134f06-ffa6-4e8d-8bf0-49f3d07a8918&text=" + text;
+
+    audio.src = url;
+    audio.load();
+    audio.play();
+
+    //var msg = new SpeechSynthesisUtterance(text);
+    //window.speechSynthesis.speak(msg);
+}
