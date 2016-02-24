c10cb8d78f8807680342fb7923ee86f6fd12d674
derwish-pro <derwish.pro@gmail.com>
2016-02-24 21:03:45 +0300

    ui button node > Generate Zero setting

--- Libs/Nodes.UI/Nodes/UiButtonNode.cs
+++ Libs/Nodes.UI/Nodes/UiButtonNode.cs
@@ -13,23 +13,31 @@ namespace MyNetSensors.Nodes
         {
             AddOutput();
             Outputs[0].Value = "0";
+
+            Settings.Add("zero", new NodeSetting(NodeSettingType.Checkbox, "Generate Zero", "true"));
         }
 
-    
+
         public override bool SetValues(Dictionary<string, string> values)
         {
             Outputs[0].Value = "1";
             UpdateMe();
 
-            Outputs[0].Value = "0";
-            UpdateMe();
+            if (Settings["zero"].Value == "true")
+            {
+                Outputs[0].Value = "0";
+                UpdateMe();
+            }
 
             return true;
         }
 
         public override string GetNodeDescription()
         {
-            return "This is a UI node. It displays a button on the dashboard.";
+            return "This is a UI node. It displays a button on the dashboard. <br/>" +
+                   "If \"Generate Zero\" option is enabled in the settings of the node, " +
+                   "node will send a \"0\" right after \"1\". " +
+                   "If the option is disabled, \"0\" will not be sent.";
         }
     }
 }
