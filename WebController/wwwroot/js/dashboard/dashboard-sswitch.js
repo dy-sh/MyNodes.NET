bb9faf05a064ad95889809249e771d6630fc5c97
dpressle <daniel_pressler@bmc.com>
2016-05-14 21:16:49 +0300

    add switch UI node with state

---
+++ WebController/wwwroot/js/dashboard/dashboard-sswitch.js
@@ -0,0 +1,23 @@
+﻿var stateSwitchTemplate = Handlebars.compile($('#stateSwitchTemplate').html());
+
+function createStateSwitch(node) {
+    $(stateSwitchTemplate(node)).hide().appendTo("#uiContainer-" + node.PanelId).fadeIn(elementsFadeTime);
+
+    $('#stateSwitch-' + node.Id).click(function () {
+        sendStateSwitchClick(node.Id);
+    });
+}
+
+function sendStateSwitchClick(nodeId) {
+    $.ajax({
+        url: "/DashboardAPI/SetValues/",
+        type: "POST",
+        data: { 'nodeId': nodeId, 'values': { click: "true" } }
+    });
+}
+
+function updateStateSwitch(node) {
+    $('#stateSwitchName-' + node.Id).html(node.Settings["Name"].Value);
+    $('#stateSwitch-' + node.Id).html(node.Settings["Name"].Value);
+    $('#stateSwitch-' + node.Id).prop('checked', node.Value == "1");
+}
